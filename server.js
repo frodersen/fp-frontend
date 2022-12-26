93445b9ca1ce1f7528aca144468f37f5d2a0ccd6
frodpede <frodpede@stud.ntnu.no>
2022-12-26 15:17:41 +0100

    Fixing the server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/frodepedersen/src/index.html');
 });
 
 app.listen(3000, function () {
